59db3e138e3160d7ecfdf2f5b5676f7e04b346bb
Gmike47 <galezra20@gmail.com>
2023-04-13 13:14:38 +0300

    fix avatar, finally

--- src/features/Avatar/Avatar.jsx
+++ src/features/Avatar/Avatar.jsx
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './Avatar.css';
+import defpic from './default.png'
 
 const Avatar = (props) => {
     const { name } = props;
-    
+    const [src, setSrc] = useState([]);
+
+    const userApi = async (name) => {
+        const response = await fetch(`https://www.reddit.com/user/${name}/about.json`);
+        const json = await response.json();
+        const data = json.data.snoovatar_img;
+        
+        if (data.length > 0) {
+            setSrc(data);
+        } else {
+            setSrc(defpic)
+        }
+    };
+
+    useEffect(() => {
+        userApi(name);
+    },[name]);
+
     return (
         <img 
-        src={`https://api.adorable.io/avatars/10/${name}`}
+        src={src}
         alt={`${name} profile`}
         className="avatar-profile-image"
         />
